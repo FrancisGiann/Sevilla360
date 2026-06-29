d326b70af5e1216ec9529ac47b45d3cb0f9b7a07
Francis Giann <francisgiann25@gmail.com>
2026-06-29 22:58:05 +0800

    changed

---
+++ config/db_connect.php
@@ -0,0 +1,24 @@
+<?php
+// 1. Set PHP Timezone
+date_default_timezone_set('Asia/Manila');
+
+// 2. Database credentials
+$host = "localhost";
+$username = "admin";
+$password = "admin123";
+$database = "sevilla360";
+
+// 3. Create the connection FIRST
+$conn = new mysqli($host, $username, $password, $database);
+
+// 4. Check if the connection worked
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// 5. NOW you can set the MySQL Timezone (because $conn successfully exists!)
+$conn->query("SET time_zone = '+08:00'");
+
+// 6. Test message (You can delete or comment this out later so it doesn't show on your website)
+echo "Connected successfully to SEVILLA360!";
+?>
